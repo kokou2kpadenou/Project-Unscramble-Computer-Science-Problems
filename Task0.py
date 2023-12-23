6b78aeaf49d57597d04cae747d65c24e4997b29f
Kokou Kpadenou <kkokou@gmail.com>
2023-12-23 05:19:12 -0500

    feat: Complete task 0

--- Task0.py
+++ Task0.py
@@ -20,3 +20,10 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+# Texts
+fisrt_texts = texts[0]
+print('First record of texts, {} texts {} at time {}'.format(fisrt_texts[0], fisrt_texts[1], fisrt_texts[2]))
+
+# Calls
+last_calls = calls[len(calls) -1]
+print('Last record of calls, {} calls {}  at time {}, lasting {} seconds'.format(last_calls[0], last_calls[1], last_calls[2], last_calls[3]))
