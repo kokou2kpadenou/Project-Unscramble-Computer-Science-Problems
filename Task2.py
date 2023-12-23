b83d06303880d1d0759e909507b32e5496cd4755
Kokou Kpadenou <kkokou@gmail.com>
2023-12-23 05:19:41 -0500

    feat: Complete task 2

--- Task2.py
+++ Task2.py
@@ -20,3 +20,29 @@ Print a message:
 September 2016.".
 """
 
+# Dictionary to hold phone number with time spent on the phone during September 2016.
+# ex: {{"phone number": "temie spent"}}
+phone_time_spent = dict()
+
+for call in calls:
+    # Caller time spent on phone
+    if call[0] in phone_time_spent:
+        phone_time_spent.update({call[0]: phone_time_spent[call[0]] + int(call[3])})
+    else:
+        phone_time_spent[call[0]] = int(call[3])
+
+    # Called time spent answering.
+    if call[1] in phone_time_spent:
+        phone_time_spent.update({call[1]: phone_time_spent[call[1]] + int(call[3])})
+    else:
+        phone_time_spent[call[1]] = int(call[3])
+
+
+longest_time = 0
+phone_number = ""
+for key, value in phone_time_spent.items():
+    if value > longest_time:
+        longest_time = value
+        phone_number = key
+
+print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(phone_number, longest_time))
