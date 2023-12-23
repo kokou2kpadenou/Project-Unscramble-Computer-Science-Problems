600d04acaec79ebf0c9994930319a40084947f6c
Kokou Kpadenou <kkokou@gmail.com>
2023-12-23 05:19:29 -0500

    feat: Complete task 1

--- Task1.py
+++ Task1.py
@@ -18,3 +18,33 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+distinct_phone = []
+
+# 
+def add_to_distinct_list(records, phone_list):
+    """
+    Add phone number from 'records' to 'phone_list' while ensuring distinct phone number.
+
+    Parameters:
+    records (list): A list of records where each record is expected to be a list.
+                    The function will consider the first two elements of each record.
+    phone_list (list): A list that will be modified to store distinct phone numbers from 'records'.
+
+    Returns:
+    None
+    """
+    for elt in records:
+        # caller/sender
+        if not elt[0] in phone_list:
+            phone_list.append(elt[0])
+        # receiver
+        if not elt[1] in phone_list:
+            phone_list.append(elt[1])
+
+# Trait texts records
+add_to_distinct_list(texts, distinct_phone)
+# Trait calls records
+add_to_distinct_list(calls, distinct_phone)
+
+print("There are {} different telephone numbers in the records.".format(len(distinct_phone)))
